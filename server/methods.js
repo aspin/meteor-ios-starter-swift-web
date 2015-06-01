77129414be58b793c5c312882417ca789c665ca2
aspin <tsunami70875@gmail.com>
2015-06-01 16:38:40 -0500

    boilerplate done

---
+++ server/methods.js
@@ -0,0 +1,16 @@
+/*
+
+    ADD IN SERVER METHODS HERE.
+
+    e.g. if you need to call a secure function from the
+    server that has full access to database.
+
+*/
+
+Meteor.methods({
+    ping: function (arg) {
+        console.log('PING!');
+        console.log(arg);
+        return 'PONG!';
+    }
+});
